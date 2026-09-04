66bc48b6a6ae8f4bf363a37986bc4bf55277f590
Tze Tsang <wg3606@gmail.com>
2026-09-03 22:41:30 -0700

    Version 1.1.8.7 restore completion popup visibility

--- Frontend/app.js
+++ Frontend/app.js
@@ -204,8 +204,6 @@ const characterCount = document.querySelector("#character-count");
 const uploadError = document.querySelector("#upload-error");
 const descriptionError = document.querySelector("#description-error");
 const formMessage = document.querySelector("#form-message");
-const formMessageSetupHome =
-  formMessage?.parentElement ?? null;
 const createButton = document.querySelector("#create-button");
 const recentProjects =
   document.querySelector(
@@ -1378,23 +1376,10 @@ function moveAudioToPlanReview() {
 
 moveAudioToSetup();
 
-function moveFormMessageToSetup() {
-  if (formMessage && formMessageSetupHome) {
-    formMessageSetupHome.append(formMessage);
-  }
-}
-
-function moveFormMessageToPlanReview() {
-  if (formMessage && planReview) {
-    planReview.append(formMessage);
-  }
-}
-
 function showPlanReviewMode() {
   moveDurationToPlanReview();
   moveStyleToPlanReview();
   moveAudioToPlanReview();
-  moveFormMessageToPlanReview();
   recentProjects.hidden = true;
   form.hidden = true;
   planReview.hidden = false;
@@ -1539,7 +1524,6 @@ function showProjectSetupMode() {
   moveDurationToSetup();
   moveStyleToSetup();
   moveAudioToSetup();
-  moveFormMessageToSetup();
   planReview.hidden = true;
   form.hidden = false;
 
